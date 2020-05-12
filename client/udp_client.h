6de61c50d607617ba764c79665bf6e7635613890
Coding your life <1596453962@qq.com>
2020-05-12 15:32:38 +0800

    Update udp_client.h

--- client/udp_client.h
+++ client/udp_client.h
@@ -12,13 +12,13 @@
 
 class udp_client{
 	public:
-		udp_client(std::string server_ip,int server_port);
-		void init_client();
-		void recv_data(std::string &out_string);
-		void send_data(const std::string &in_string);
-		~udp_client();
+		udp_client(std::string server_ip,int server_port);//client要监听的server的ip和端口号,为构造函数
+		void init_client();//初始化client
+		void recv_data(std::string &out_string);//发送数据
+		void send_data(const std::string &in_string);//接收数据
+		~udp_client();//为析构函数
 	private:
-		int sock;
-		struct sockaddr_in server;
+		int sock;//
+		struct sockaddr_in server;//定义一个server结构体
 };
 
